bfa1c097b832ccb1be96983e1f7d9c024da5a531
Seungjae Jeon <seungjae5044@gmail.com>
2024-11-22 14:04:06 +0900

    Read me 수정

--- Shooting/main.py
+++ Shooting/main.py
@@ -8,6 +8,7 @@ from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
 
 
+
 # This program requires LEGO EV3 MicroPython v2.0 or higher.
 # Click "Open user guide" on the EV3 extension tab for more information.
 
